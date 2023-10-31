47103ef0ffefa1870b86e3c9ef2373159c4124af
Shut-inNEET <112591173+Shut-inNEET@users.noreply.github.com>
2023-10-31 00:59:52 +0000

    Cleaned up syntax

--- main.cpp
+++ main.cpp
@@ -8,7 +8,10 @@ using namespace std;
 int main() 
 {
   calendarType calendar(1, 2022); // Creates a calendar for January 2022
+  calendar.printCalendar(); // Prints the calendar
   
+  calendar.setMonth(2); // Set the month to Febuary
+  calendar.setYear(2024); // Set the year to 2024
   calendar.printCalendar(); // Print the calendar
   
   return 0;
