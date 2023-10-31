bef9fd601bb314598a2056cea4bde73c5a83a62d
Shut-inNEET <112591173+Shut-inNEET@users.noreply.github.com>
2023-10-31 01:16:07 +0000

    Changed the main function to take multiple inputs

--- .ccls-cache/@home@runner@Calendar-Project-ShutInNEET/main.cpp
+++ .ccls-cache/@home@runner@Calendar-Project-ShutInNEET/main.cpp
@@ -7,12 +7,23 @@ using namespace std;
 
 int main() 
 {
-  calendarType calendar(1, 2022); // Creates a calendar for January 2022
-  calendar.printCalendar(); // Prints the calendar
+  int month, year;
   
-  calendar.setMonth(2); // Set the month to Febuary
-  calendar.setYear(2024); // Set the year to 2024
-  calendar.printCalendar(); // Print the calendar
+  while(true) 
+  {
+    cout << "Enter the month (1-12) and the year (YYYY) (enter -1 -1 to quit): ";
+    cin >> month >> year;
+    cout << endl;
+    
+    if(month == -1 && year == -1)
+    {
+      break; // Exit the loop
+    }
+    
+    calendarType calendar(month, year); // Creates a calendar for user-defined month and year
+    calendar.printCalendar(); // Print the calendar
+  }
+  cout << endl;
   
   return 0;
 }
